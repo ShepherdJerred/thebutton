e81b4e54f406c237b8f961988fb8aeb2a5a132b3
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-02-17 13:04:43 -0600

    Add basic vue component

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,10 +1,7 @@
 var app = new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
 });
 
-Vue.component('navigation', {
-    template: "<h1>Navigation</h1>"
+Vue.component('thebutton', {
+    template: '<div class="button"></div>'
 });
