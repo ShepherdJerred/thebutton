9e4d4c9d9e545125270fdb67889ce764437404b2
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-02-17 14:48:12 -0600

    Make some more components

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,7 +1,19 @@
-var app = new Vue({
-    el: '#app',
+Vue.component('app-container', {
+    template: '<div class="pure-g"><div class="pure-u-1-4"></div><div class="pure-u-1-2"><app-button-container></app-button-container><app-progress></app-progress></div></div>'
+})
+
+Vue.component('app-button-container', {
+    template: '<div class="button-container"><app-button></app-button></div>'
 });
 
-Vue.component('thebutton', {
-    template: '<div class="button"></div>'
+Vue.component('app-button', {
+    template: '<div class="button-outer"><div class="button-inner"></div></div>'
+});
+
+Vue.component('app-progress', {
+    template: '<div class="button-progress"><div class="button-progress-inner"></div></div>'
+});
+
+var app = new Vue({
+    el: '#app'
 });
