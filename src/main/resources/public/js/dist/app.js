79b53b75740e555986b42e6c4ea447fa138a7810
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-02-23 21:17:14 -0600

    Remove link for now

--- src/main/resources/public/js/dist/app.js
+++ src/main/resources/public/js/dist/app.js
@@ -108,7 +108,7 @@ Vue.component('sj-footer', {
 });
 
 var thebutton = {
-    template: "<div><app-container></app-container><div class='pure-u-1'><app-users></app-users></div><div class='pure-u-1'><div class='link-wrapper'><router-link to='/leaderboard' class='button'>Leaderboard</router-link></div></div></div>"
+    template: "<div><app-container></app-container><div class='pure-u-1'><app-users></app-users></div><div class='pure-u-1'></div></div>"
 
 };
 
@@ -125,14 +125,6 @@ var router = new VueRouter({
 var app = new Vue({
     router: router
 }).$mount("#app");
-
-//# sourceMappingURL=app.js.map
-
-//# sourceMappingURL=app.js.map
-
-//# sourceMappingURL=app.js.map
-
-//# sourceMappingURL=app.js.map
 
 //# sourceMappingURL=app.js.map
 
