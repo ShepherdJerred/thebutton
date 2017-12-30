ab9c653362ad767d52e843084e2b9cf4e4666853
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-12-29 19:50:26 -0500

    Allow CORS

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ connection.query('CREATE TABLE IF NOT EXISTS counter (counter_uuid CHAR(36), cur
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./app/routes')(app, connection);
 
 app.use(bodyParser.urlencoded({extended: true}));
